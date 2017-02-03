77df0837d1315957a846975881179c5eaf4aea6a
George Nguyen <g.nguyen92@yahoo.com>
2017-02-02 17:22:19 -0800

    Add japanese phrases and corresponding xml file to display WordAdapter list

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("Hello", "Konichiwa"));
+        words.add(new Word("Goodbye", "Sayonara"));
+        words.add(new Word("Good morning", "Ohayo gozaimasu"));
+        words.add(new Word("Good night", "Oyasuminasai"));
+        words.add(new Word("How are you?", "Ogenki desu ka"));
+        words.add(new Word("Thank you", "Waza waza honto ni arigato gozaimasu!"));
+        words.add(new Word("Please?", "Dozo"));
+        words.add(new Word("You're welcome", "Do itashimashite"));
+        words.add(new Word("Excuse me", "Sumimasen"));
+        words.add(new Word("When?", "Itsu?"));
+        words.add(new Word("Where?", "Doko?"));
+        words.add(new Word("Who?", "Dare?"));
+        words.add(new Word("I don't understand", "Wakarimasen"));
+        words.add(new Word("How much is it?", "Ikura desu ka?"));
+        words.add(new Word("Where is the toilet?", "Toire wa doko desu ka?"));
+
+        // Adding a WordAdapter list of word pairs with dividers
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
