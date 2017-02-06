6dc0c013f4623fc60dda5dcb4829512d3ea4e057
George Nguyen <g.nguyen92@yahoo.com>
2017-02-06 15:42:07 -0800

    Add number icons and update icons in NumbersActivity

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Pre-requisites
 - Android Build Tools v23.0.2
 - Android Support Repository v23.3.0
 
+
+Credits
+-------
+
+- Number Icons:
+	<div>Icons made by <a href="http://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,27 +15,27 @@ public class NumbersActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("one","ichi", R.drawable.number_one));
-        words.add(new Word("two","ni", R.drawable.number_two));
-        words.add(new Word("three","san", R.drawable.number_three));
-        words.add(new Word("four","shi/yon", R.drawable.number_four));
-        words.add(new Word("five","go", R.drawable.number_five));
-        words.add(new Word("six","roku", R.drawable.number_six));
-        words.add(new Word("seven","schichi/nana", R.drawable.number_seven));
-        words.add(new Word("eight","hachi", R.drawable.number_eight));
-        words.add(new Word("nine","kyu", R.drawable.number_nine));
-        words.add(new Word("ten","ju", R.drawable.number_ten));
-        words.add(new Word("eleven","ju ichi", R.mipmap.ic_launcher));
-        words.add(new Word("twelve","ju ni", R.mipmap.ic_launcher));
-        words.add(new Word("thirteen","ju san", R.mipmap.ic_launcher));
-        words.add(new Word("fourteen","ju yon", R.mipmap.ic_launcher));
-        words.add(new Word("fifteen","ju go", R.mipmap.ic_launcher));
-        words.add(new Word("twenty","ni-ju", R.mipmap.ic_launcher));
-        words.add(new Word("thirty","san-ju", R.mipmap.ic_launcher));
-        words.add(new Word("forty","yon-ju", R.mipmap.ic_launcher));
-        words.add(new Word("one hundred","hyaku", R.mipmap.ic_launcher));
-        words.add(new Word("two hundred","nihyaku", R.mipmap.ic_launcher));
-        words.add(new Word("three hundred","sanbyaku", R.mipmap.ic_launcher));
+        words.add(new Word("one","ichi", R.mipmap.number_1));
+        words.add(new Word("two","ni", R.mipmap.number_2));
+        words.add(new Word("three","san", R.mipmap.number_3));
+        words.add(new Word("four","shi/yon", R.mipmap.number_4));
+        words.add(new Word("five","go", R.mipmap.number_5));
+        words.add(new Word("six","roku", R.mipmap.number_6));
+        words.add(new Word("seven","schichi/nana", R.mipmap.number_7));
+        words.add(new Word("eight","hachi", R.mipmap.number_8));
+        words.add(new Word("nine","kyu", R.mipmap.number_9));
+        words.add(new Word("ten","ju", R.mipmap.number_10));
+        //words.add(new Word("eleven","ju ichi", R.mipmap.ic_launcher));
+        //words.add(new Word("twelve","ju ni", R.mipmap.ic_launcher));
+        //words.add(new Word("thirteen","ju san", R.mipmap.ic_launcher));
+        //words.add(new Word("fourteen","ju yon", R.mipmap.ic_launcher));
+        //words.add(new Word("fifteen","ju go", R.mipmap.ic_launcher));
+        words.add(new Word("twenty","ni-ju", R.mipmap.number_20));
+        words.add(new Word("thirty","san-ju", R.mipmap.number_30));
+        words.add(new Word("forty","yon-ju", R.mipmap.number_40));
+        words.add(new Word("one hundred","hyaku", R.mipmap.number_100));
+        //words.add(new Word("two hundred","nihyaku", R.mipmap.ic_launcher));
+        //words.add(new Word("three hundred","sanbyaku", R.mipmap.ic_launcher));
 
     // Adding a WordAdapter list of word pairs with dividers
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
