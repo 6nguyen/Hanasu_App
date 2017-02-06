7826747c05ccaa281a24d5141cd669ab30d6f8bc
George Nguyen <g.nguyen92@yahoo.com>
2017-02-05 20:19:09 -0800

    Add color images to ColorsActivity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,14 +15,14 @@ public class ColorsActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("red", "aka", R.mipmap.ic_launcher));
-        words.add(new Word("green", "midori iro", R.mipmap.ic_launcher));
-        words.add(new Word("brown", "cha iro", R.mipmap.ic_launcher));
-        words.add(new Word("grey", "nezumi iro", R.mipmap.ic_launcher));
-        words.add(new Word("black", "kuro", R.mipmap.ic_launcher));
-        words.add(new Word("white", "shiro", R.mipmap.ic_launcher));
-        words.add(new Word("beige", "be-ju", R.mipmap.ic_launcher));
-        words.add(new Word("mustard yellow", "oudo iro", R.mipmap.ic_launcher));
+        words.add(new Word("red", "aka", R.drawable.color_red));
+        words.add(new Word("green", "midori iro", R.drawable.color_green));
+        words.add(new Word("brown", "cha iro", R.drawable.color_brown));
+        words.add(new Word("grey", "nezumi iro", R.drawable.color_gray));
+        words.add(new Word("black", "kuro", R.drawable.color_black));
+        words.add(new Word("white", "shiro", R.drawable.color_white));
+        words.add(new Word("beige", "be-ju", R.drawable.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "oudo iro", R.drawable.color_mustard_yellow));
 
 
 
