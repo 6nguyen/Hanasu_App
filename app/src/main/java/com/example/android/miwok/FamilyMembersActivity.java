f40738530582ff46fe640ef70de006b792a3b9e8
George Nguyen <g.nguyen92@yahoo.com>
2017-02-05 22:03:08 -0800

    Add images to FamilyMembersActivity

--- app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -15,18 +15,18 @@ public class FamilyMembersActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("father","Otoo-san", R.mipmap.ic_launcher));
-        words.add(new Word("mother","Okaa-san", R.mipmap.ic_launcher));
-        words.add(new Word("son","musuko", R.mipmap.ic_launcher));
-        words.add(new Word("daughter","musume", R.mipmap.ic_launcher));
-        words.add(new Word("older brother","Onii-san", R.mipmap.ic_launcher));
-        words.add(new Word("younger brother","Otouto", R.mipmap.ic_launcher));
-        words.add(new Word("older sister","Onee-san", R.mipmap.ic_launcher));
-        words.add(new Word("younger sister","Imouto", R.mipmap.ic_launcher));
-        words.add(new Word("grandmother","Obaa-san", R.mipmap.ic_launcher));
-        words.add(new Word("grandfather","Ojii-san", R.mipmap.ic_launcher));
-        words.add(new Word("aunt","Oba-san", R.mipmap.ic_launcher));
-        words.add(new Word("uncle","Oji-san", R.mipmap.ic_launcher));
+        words.add(new Word("father","Otoo-san", R.drawable.family_father));
+        words.add(new Word("mother","Okaa-san", R.drawable.family_mother));
+        words.add(new Word("son","musuko", R.drawable.family_son));
+        words.add(new Word("daughter","musume", R.drawable.family_daughter));
+        words.add(new Word("older brother","Onii-san", R.drawable.family_older_brother));
+        words.add(new Word("younger brother","Otouto", R.drawable.family_younger_brother));
+        words.add(new Word("older sister","Onee-san", R.drawable.family_older_sister));
+        words.add(new Word("younger sister","Imouto", R.drawable.family_younger_sister));
+        words.add(new Word("grandmother","Obaa-san", R.drawable.family_grandmother));
+        words.add(new Word("grandfather","Ojii-san", R.drawable.family_grandfather));
+        words.add(new Word("aunt","Oba-san", R.drawable.family_mother));
+        words.add(new Word("uncle","Oji-san", R.drawable.family_father));
 
 
         // Adding a WordAdapter list of word pairs with dividers
