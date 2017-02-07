e0971a0f85cc899b389824662cbc6b5df985a41f
George Nguyen <g.nguyen92@yahoo.com>
2017-02-07 13:52:49 -0800

    Add play icon into ColorsActivity and PhrasesActivity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,16 +15,16 @@ public class ColorsActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("red", "aka", R.mipmap.color_red));
-        words.add(new Word("green", "midori iro", R.mipmap.color_green));
-        words.add(new Word("pink", "pinku", R.mipmap.color_pink));
-        words.add(new Word("yellow", "oudo iro", R.mipmap.color_yellow));
-        words.add(new Word("blue", "ao", R.mipmap.color_blue));
-        words.add(new Word("grey", "nezumi iro", R.drawable.color_gray));
-        words.add(new Word("black", "kuro", R.drawable.color_black));
-        words.add(new Word("white", "shiro", R.drawable.color_white));
-        words.add(new Word("beige", "be-ju", R.drawable.color_dusty_yellow));
-        words.add(new Word("brown", "cha iro", R.drawable.color_brown));
+        words.add(new Word("red", "aka", R.mipmap.color_red,R.raw.song));
+        words.add(new Word("green", "midori iro", R.mipmap.color_green,R.raw.song));
+        words.add(new Word("pink", "pinku", R.mipmap.color_pink,R.raw.song));
+        words.add(new Word("yellow", "oudo iro", R.mipmap.color_yellow,R.raw.song));
+        words.add(new Word("blue", "ao", R.mipmap.color_blue,R.raw.song));
+        words.add(new Word("grey", "nezumi iro", R.drawable.color_gray,R.raw.song));
+        words.add(new Word("black", "kuro", R.drawable.color_black,R.raw.song));
+        words.add(new Word("white", "shiro", R.drawable.color_white,R.raw.song));
+        words.add(new Word("beige", "be-ju", R.drawable.color_dusty_yellow,R.raw.song));
+        words.add(new Word("brown", "cha iro", R.drawable.color_brown,R.raw.song));
 
 
 
