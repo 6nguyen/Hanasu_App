aabab5e22c2be7e2d2e81d355ea08c3007ed39bd
George Nguyen <g.nguyen92@yahoo.com>
2017-02-07 14:41:18 -0800

    Add mAudioResourceId into FamilyMembersActivity so both family icons and play icon are displayed

--- app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -15,18 +15,18 @@ public class FamilyMembersActivity extends AppCompatActivity {
 
         ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("father","Otoo-san", R.mipmap.father));
-        words.add(new Word("mother","Okaa-san", R.mipmap.mother));
-        words.add(new Word("son","musuko", R.mipmap.son));
-        words.add(new Word("daughter","musume", R.mipmap.daughter));
-        words.add(new Word("older brother","Onii-san", R.mipmap.son));
-        words.add(new Word("younger brother","Otouto", R.mipmap.younger_brother));
-        words.add(new Word("older sister","Onee-san", R.mipmap.daughter));
-        words.add(new Word("younger sister","Imouto", R.mipmap.younger_sister));
-        words.add(new Word("grandmother","Obaa-san", R.mipmap.grandma));
-        words.add(new Word("grandfather","Ojii-san", R.mipmap.grandpa));
-        words.add(new Word("aunt","Oba-san", R.mipmap.aunt));
-        words.add(new Word("uncle","Oji-san", R.mipmap.uncle));
+        words.add(new Word("father","Otoo-san", R.mipmap.father,R.raw.song));
+        words.add(new Word("mother","Okaa-san", R.mipmap.mother,R.raw.song));
+        words.add(new Word("son","musuko", R.mipmap.son,R.raw.song));
+        words.add(new Word("daughter","musume", R.mipmap.daughter,R.raw.song));
+        words.add(new Word("older brother","Onii-san", R.mipmap.son,R.raw.song));
+        words.add(new Word("younger brother","Otouto", R.mipmap.younger_brother,R.raw.song));
+        words.add(new Word("older sister","Onee-san", R.mipmap.daughter,R.raw.song));
+        words.add(new Word("younger sister","Imouto", R.mipmap.younger_sister,R.raw.song));
+        words.add(new Word("grandmother","Obaa-san", R.mipmap.grandma,R.raw.song));
+        words.add(new Word("grandfather","Ojii-san", R.mipmap.grandpa,R.raw.song));
+        words.add(new Word("aunt","Oba-san", R.mipmap.aunt,R.raw.song));
+        words.add(new Word("uncle","Oji-san", R.mipmap.uncle,R.raw.song));
 
 
         // Adding a WordAdapter list of word pairs with dividers
