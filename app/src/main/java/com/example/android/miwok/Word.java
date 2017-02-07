f828e416426656385e41c1c451f6c9d6f820700d
George Nguyen <g.nguyen92@yahoo.com>
2017-02-07 13:41:20 -0800

    Add mAudioResourceId to Word.java file and update constructors

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+
 /**
  * Created by gnguy on 2/1/2017.
  */
@@ -9,15 +10,17 @@ public class Word {
     private String mEnglishWord;
     private String mJapaneseWord;
     private int mImageResourceId = -1;
+    private int mAudioResrouceId;
 
     /** Constructor with NO image
      *  Create a Word Object instance containing a String's Japanese and English translation
      * @param eng English translation
      * @param jap Japanese translation
      */
-    public Word(String eng, String jap){
+    public Word(String eng, String jap, int AudioResrouceId){
         mEnglishWord = eng;
         mJapaneseWord = jap;
+        mAudioResrouceId = AudioResrouceId;
     }
 
     /** Constructor with image
@@ -27,10 +30,11 @@ public class Word {
      * @param jap Japanese translation
      * @param imageResourceId Resource ID reference to actual image's location, ie R.drawable.num_1
      */
-    public Word(String eng, String jap, int imageResourceId){
+    public Word(String eng, String jap, int imageResourceId, int AudioResrouceId){
         mEnglishWord = eng;
         mJapaneseWord = jap;
         mImageResourceId = imageResourceId;
+        mAudioResrouceId = AudioResrouceId;
     }
 
     /** Accessor Methods
