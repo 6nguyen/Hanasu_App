bf9054231c0f7cb873604a0659f5140139857614
George Nguyen <g.nguyen92@yahoo.com>
2017-02-27 23:14:47 -0800

    Change Politeness and Questions activities into fragments.

--- app/src/main/java/com/gnguyen/android/hanasu/fragmentPoliteness.java
+++ app/src/main/java/com/gnguyen/android/hanasu/fragmentPoliteness.java
@@ -1,29 +1,27 @@
 package com.gnguyen.android.hanasu;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 /**
- * Created by gnguy on 2/17/2017.
+ * Created by gnguy on 2/27/2017.
  */
 
-public class PolitenessActivity extends AppCompatActivity {
+public class fragmentPoliteness  extends Fragment{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new fragmentPoliteness()).commit();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
 
-
-        /**
         ArrayList<Word> words = new ArrayList<Word>();
 
-        /** Politeness
+        /** Politeness */
         words.add(new Word("Thank you", "Doumo arigato gozaimasu!", R.raw.doumo_arigato_gozaimasu));
         words.add(new Word("You're welcome", "Dou itashimashite", R.raw.dou_itashimashite));
         words.add(new Word("Sorry", "Gomennasai", R.raw.gomennasai));
@@ -33,10 +31,10 @@ public class PolitenessActivity extends AppCompatActivity {
 
 
         // Adding a WordAdapter list of word pairs with dividers
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_politeness);
-        ListView listView = (ListView) findViewById(R.id.list);
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_politeness);
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
         listView.setAdapter(adapter);
 
-         */
+        return rootView;
     }
 }
--- app/src/main/java/com/gnguyen/android/hanasu/fragmentQuestions.java
+++ app/src/main/java/com/gnguyen/android/hanasu/fragmentQuestions.java
@@ -1,27 +1,27 @@
 package com.gnguyen.android.hanasu;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 /**
- * Created by gnguy on 2/17/2017.
+ * Created by gnguy on 2/27/2017.
  */
 
-public class QuestionsActivity extends AppCompatActivity {
-
+public class fragmentQuestions extends Fragment {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new fragmentQuestions()).commit();
-/**
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
         ArrayList<Word> words = new ArrayList<Word>();
 
-        /** Questions
+        /** Questions */
         words.add(new Word("When is it?", "Itsu?", R.raw.itsu));
         words.add(new Word("Where is that?", "Sore wa doko desu ka?", R.raw.phrases_sore_wa_doko_desu_ka));
         words.add(new Word("Who is that?", "Are wa dare?", R.raw.phrases_are_wa_dare));
@@ -30,11 +30,11 @@ public class QuestionsActivity extends AppCompatActivity {
         words.add(new Word("How much is it?", "Ikura desu ka?", R.raw.ikura_desu_ka));
         words.add(new Word("Do you understand?", "Wakarimasu ka?", R.raw.wakarimasu_ka));
 
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_questions);
-        ListView listView = (ListView) findViewById(R.id.list);
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_questions);
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
         listView.setAdapter(adapter);
 
- */
+        return rootView;
     }
-
 }
+
